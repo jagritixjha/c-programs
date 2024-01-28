0dabb7c103e463d3855fa21c0f4c3b7c4bad2c4a
jagriti <im.jagriti.jha@gmail.com>
2024-01-28 17:32:26 +0530

    Basic program

---
+++ Basics/variable.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+
+{  // Create variables
+  int myNum = 5;
+  float myFloatNum = 5.999;
+  char myLetter = 'D';
+
+ // Print variables
+  printf("%d\n", myNum);
+  printf("%f\n", myFloatNum);
+  printf("%c\n", myLetter);
+  printf("My number is %d and my letter is %c", myNum, myLetter);
+  return 0;
+}
