0d7202e627f77fb03efdf3f2478351c86629787e
jagriti <im.jagriti.jha@gmail.com>
2024-01-28 17:40:04 +0530

    Reapply "Basic program"
    This reverts commit 0c0cec3b648d7d686f51faff8a7a73994768242d.

---
+++ Basics/pattern.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+  printf("*\n**\n***\n****\n");
+  printf(" **************************\n");
+  printf("|       JAGRITI JHA        |\n");
+  printf("|                          |\n");
+  printf("|                          |\n");
+  printf("|                          |\n");
+  printf("----------------------------\n");
+  printf("|                          |\n");
+  printf("|                          |\n");
+  printf("|                          |\n");
+  printf("|                          |\n");
+  printf(" **************************\n");
+  
+  return 0;
+
+}
+    
---
+++ Basics/while.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  int a = 30;
+  while (a < 20)
+  {
+    printf("the vale of a is %d\n", a);
+    a++;
+  }
+  return 0;
+}
