b276ce4022a979d4e42be4506aa08f7fb8e371fd
jagriti <im.jagriti.jha@gmail.com>
2024-02-02 21:57:20 +0530

    Conditional statement

--- conditional/switchCase-goto2.c
+++ conditional/switchCase-goto2.c
@@ -1,26 +1,27 @@
-// mathematical operations using switch case & int user input.
+// mathematical operations using switch case using character user input & case insensitive.
 // demonstates th use of switch case, label, goto.
 
 #include <stdio.h>
 
-void case1();
-void case3();
+void case2();
 
 int main()
 {
-  case1();
+  case2();
   return 0;
 }
 
-void case1() // case 1
+void case2() // case 2
 {
-  int ch, a, b;
+  int a, b;
+  char ch;
 TARGET: // label
 
-  printf("\nOperation that can be performed:\n1.Addition\t2.Subtraction\t3.Multiplication\t4.Division\nEnter the operation: ");
-  scanf("%d", &ch);
+  printf("\nOperation that can be performed:\na.Addition\tb.Subtraction\tc.Multiplication\nEnter the operation: ");
+  fflush(stdin);
+  scanf("%c", &ch);
 
-  if (ch >= 1 && ch <= 4)
+  if (ch == 'a' || ch == 'b' || ch == 'c')
   {
     printf("Enter value of a: ");
     scanf("%d", &a);
@@ -29,23 +30,20 @@ TARGET: // label
   }
   else
   {
-    printf("\nPlease enter the correct operation to be performed.\n");
+    printf("Please enter the correct operation to be performed.\n");
     goto TARGET;
   }
 
   switch (ch)
   {
-  case 1:
+  case 'a':
     printf("%d + %d = %d", a, b, a + b);
     break;
-  case 2:
+  case 'b':
     printf("%d - %d = %d", a, b, (a - b));
     break;
-  case 3:
+  case 'c':
     printf("%d * %d = %d", a, b, (a * b));
-    break;
-  case 4:
-    printf("%d / %d = %d", a, b, (a / b));
     break;
   default:
     printf("Invalid Choice!");
