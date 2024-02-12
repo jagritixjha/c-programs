9d3984741622d2469c87487eccea5ddd992bf281
jagriti <im.jagriti.jha@gmail.com>
2024-02-12 22:52:52 +0530

    array program

---
+++ Arrays/array-Concatenation.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+  int s1, s2, i, j;
+
+  printf("Enter size of array 1: ");
+  scanf("%d", &s1);
+  int a[s1];
+  for (i = 0; i < s1; i++)
+  {
+    printf("a[%d] = ", i);
+    scanf("%d", &a[i]);
+  }
+
+  printf("Enter size of array 2: ");
+  scanf("%d", &s2);
+  int b[s2];
+  for (i = 0; i < s2; i++)
+  {
+    printf("b[%d] = ", i);
+    scanf("%d", &b[i]);
+  }
+
+  int s3 = s1 + s2, c[s3];
+
+  for (int i = 0; i < s1; i++)
+  {
+    c[i] = a[i];
+  }
+
+  for (int i = 0; i < s2; i++)
+  {
+    c[s1 + i] = b[i];
+  }
+
+  for (i = 0; i < s3; i++)
+  {
+    printf("\nc[%d] = %d", i, c[i]);
+  }
+  return 0;
+}
