339b0dccafdb2f542239b141e46bbe9dc7ed1ccd
jagriti <im.jagriti.jha@gmail.com>
2024-02-17 23:48:17 +0530

    array program

--- Arrays/deletion-operation.c
+++ Arrays/deletion-operation.c
@@ -23,10 +23,10 @@ void main()
     temp = temp + 1;
   }
   n = n - 1;
-  printf("print array elements:");
+  printf("print array elements:\n");
   for (i = 0; i < n; i++)
   {
-    printf("a[%d]=%d\n", i, a[i]);
+    printf("a[%d] = %d\n", i, a[i]);
   }
   getch();
 }
