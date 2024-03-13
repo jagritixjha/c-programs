f91f2bf3908b997f56cc8f46d523a3fcf20a0de9
jagriti <im.jagriti.jha@gmail.com>
2024-03-13 23:17:39 +0530

    pointer program

---
+++ pointers/show-array-of-pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+
+  int arr[5] = {3, 5, 7, 9, 11};
+
+  printf("2227010701049\n");
+  printf("arr : %u, Value : %d\n", arr, *(arr + 1));
+  printf("&arr : %u, Value : %d\n", &arr, *(arr));
+  printf("&arr[0] : %u, Value : %d\n", &arr[0], *(&arr[0]));
+
+  getch();
+}
