b029ee685a8a1af407f8a6f3c0e163e3d81c5f5b
jagriti <im.jagriti.jha@gmail.com>
2024-03-14 23:09:03 +0530

    pointer program

---
+++ pointers/ptr2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  int a, b, *x, *y;
+  x = &a;
+  y = &b;
+
+  a = 10;
+  b = 70;
+  printf("The addition of %d and %d is %d", *x, *y, (*x + *y));
+  return 0;
+}
---
+++ pointers/ptr4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int add(int *x, int *y)
+{
+  return (*x + *y);
+}
+
+int main()
+{
+  int a = 10, b = 50, ans;
+  ans = add(&a, &b);
+  printf("The addition of %d and %d is %d.\n", a, b, ans);
+  return 0;
+}
